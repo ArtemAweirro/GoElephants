a318bd2c82811661041acd64aa9fb29bb51bd2e4
ArtemAweirro <sixvart05@bk.ru>
2026-01-27 17:58:59 +0600

    build: add hotReload and buildkonfig

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -1,5 +1,10 @@
+import org.jetbrains.compose.reload.gradle.ComposeHotRun
+import org.jetbrains.kotlin.compose.compiler.gradle.ComposeFeatureFlag
+
 plugins {
     id("ru.caravanlabs.elephants.convention.application")
+    alias(libs.plugins.composeHotReload)
+    alias(libs.plugins.buildkonfig)
 }
 
 kotlin {
@@ -13,6 +18,23 @@ kotlin {
         commonTest.dependencies {
             implementation(libs.kotlin.test)
         }
+    }
+}
+
+// https://github.com/JetBrains/compose-hot-reload
+composeCompiler {
+    featureFlags.add(ComposeFeatureFlag.OptimizeNonSkippingGroups)
+}
+tasks.withType<ComposeHotRun>().configureEach {
+    mainClass.set("MainKt")
+}
+
+// https://github.com/yshrsmz/BuildKonfig
+buildkonfig {
+    packageName = "ru.caravanlabs.elephants"
+    exposeObjectWithName = "BuildKonfig"
+    defaultConfigs {
+
     }
 }
 
